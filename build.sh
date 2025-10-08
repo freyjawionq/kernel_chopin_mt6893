11cc83c7e5b580fb06b36c78058bf7fa017490cd
maoyihao <maoyihao2005@gmail.com>
2025-10-08 14:10:49 +0800

    Initialize build.sh

--- build.sh
+++ build.sh
@@ -7,18 +7,17 @@ source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=MARKxDEVS
-export KBUILD_BUILD_USER="AbzRaider"
-git clone --depth=1  https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13  clang
-
-
+export KBUILD_BUILD_HOST=Laptop-MaoYihao
+export KBUILD_BUILD_USER="maoyihao"
+export LOCALVERSION="-Ciallo~(∠・ω< )"
+git clone --depth=1  https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13 clang
 
  if ! [ -d "out" ]; then
 echo "Kernel OUT Directory Not Found . Making Again"
 mkdir out
 fi
 
-make O=out ARCH=arm64 ares_user_defconfig
+make O=out ARCH=arm64 chopin_user_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -36,15 +35,4 @@ make -j$(nproc --all) O=out \
    		      OBJDUMP=llvm-objdump \
                       CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
-
-function zupload()
-{
-rm -rf AnyKernel	
-git clone --depth=1 https://github.com/AbzRaider/AnyKernel33 -b ares AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-cd AnyKernel
-zip -r9 Test-OSS-KERNEL-ARES-S.zip *
-curl --upload-file "Test-OSS-KERNEL-ARES-S.zip" https://free.keep.sh
-}
 compile
-zupload
