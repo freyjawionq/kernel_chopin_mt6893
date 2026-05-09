ce5a585f75e0712c67f1df87df567e1b4e8bf3ad
firenza <masudtxg@gmail.com>
2026-05-09 15:38:43 +0700

    net: prefer BBRv3 over BBRv2 in bic redirect

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -219,8 +219,12 @@ static int proc_tcp_congestion_control(struct ctl_table *ctl, int write,
 	ret = proc_dostring(&tbl, write, buffer, lenp, ppos);
 	if (write && ret == 0) {
 		rcu_read_lock();
-		if (!strcmp(val, "bic") && tcp_ca_find("bbr2"))
-			strncpy(val, "bbr2", TCP_CA_NAME_MAX);
+		if (!strcmp(val, "bic")) {
+			if (tcp_ca_find("bbr3"))
+				strncpy(val, "bbr3", TCP_CA_NAME_MAX);
+			else if (tcp_ca_find("bbr2"))
+				strncpy(val, "bbr2", TCP_CA_NAME_MAX);
+		}
 		rcu_read_unlock();
 		ret = tcp_set_default_congestion_control(net, val);
 	}
