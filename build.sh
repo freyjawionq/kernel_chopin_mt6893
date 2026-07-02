1fc82e7651c140cd50305ab9a83d091426f47f83
freyjawionq <freyjawionq@github.com>
2026-07-02 18:38:34 +0000

    build: restore Proton Clang toolchain paths to resolve touch screen unresponsiveness

--- build.sh
+++ build.sh
@@ -20,18 +20,15 @@ function compile()
 
     make O=out ARCH=arm64 chopin_defconfig
 
-    CLANG_DIR="/home/afifnaxxwahana3/toolchains/aosp-clang"
-    GCC_DIR="/home/afifnaxxwahana3/toolchains/arm-gnu"
-
-    PATH="${CLANG_DIR}/bin:${GCC_DIR}/bin:${PATH}" \
+    PATH="${PWD}/clang/bin:${PATH}" \
     make -j$(nproc --all) O=out \
                           ARCH=arm64 \
                           CC="clang" \
                           HOSTCC="gcc" \
                           HOSTLD="ld" \
-                          CLANG_TRIPLE=aarch64-none-linux-gnu- \
-                          CROSS_COMPILE="aarch64-none-linux-gnu-" \
-                          CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+                          CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                          CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
                           LD=ld.lld \
                           STRIP=llvm-strip \
                           AS=llvm-as \
