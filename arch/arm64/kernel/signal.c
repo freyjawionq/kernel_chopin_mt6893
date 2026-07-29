84ee021633734cdced38035a8453f19cb42cfbc3
freyjawionq <freyjawionq@users.noreply.github.com>
2026-07-29 23:18:10 +0700

    fix(vdso): generate vdso_offset___kernel_rt_sigreturn and use it directly in signal.c

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -582,8 +582,10 @@ static void setup_return(struct pt_regs *regs, struct k_sigaction *ka,
 
 	if (ka->sa.sa_flags & SA_RESTORER)
 		sigtramp = ka->sa.sa_restorer;
-	else
-		sigtramp = VDSO_SYMBOL(current->mm->context.vdso, sigtramp);
+	else {
+		unsigned long vdso_base = (unsigned long)current->mm->context.vdso;
+		sigtramp = (__sigrestore_t)(vdso_base + vdso_offset___kernel_rt_sigreturn);
+	}
 
 	regs->regs[30] = (unsigned long)sigtramp;
 }
--- arch/arm64/kernel/vdso/gen_vdso_offsets.sh
+++ arch/arm64/kernel/vdso/gen_vdso_offsets.sh
@@ -12,5 +12,6 @@
 #
 
 LC_ALL=C
-sed -n -e 's/^00*/0/' -e \
-'s/^\([0-9a-fA-F]*\) . VDSO_\([a-zA-Z0-9_]*\)$/\#define vdso_offset_\2\t0x\1/p'
+sed -n -e 's/^00*/0/' \
+	-e 's/^\([0-9a-fA-F]*\) . VDSO_\([a-zA-Z0-9_]*\)$/#define vdso_offset_\2\t0x\1/p' \
+	-e 's/^\([0-9a-fA-F]*\) . \(__kernel_[a-zA-Z0-9_]*\)$/#define vdso_offset_\2\t0x\1/p'
