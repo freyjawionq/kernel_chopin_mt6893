9f6c1b579b61889673e44573e3ff0aa39fc5f8d1
freyjawionq <freyjawionq@users.noreply.github.com>
2026-07-29 23:05:22 +0700

    fix(signal/vdso): add vdso_offset_sigtramp fallback and fix sigtramp cast in signal.c

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -583,7 +583,8 @@ static void setup_return(struct pt_regs *regs, struct k_sigaction *ka,
 	if (ka->sa.sa_flags & SA_RESTORER)
 		sigtramp = ka->sa.sa_restorer;
 	else
-		sigtramp = VDSO_SYMBOL(current->mm->context.vdso, sigtramp);
+		sigtramp = (__sigrestore_t)(void *)(vdso_offset_sigtramp - 0x0UL +
+			(unsigned long)current->mm->context.vdso);
 
 	regs->regs[30] = (unsigned long)sigtramp;
 }
