e9a641be21601dd298e571e6f02fae5b4578b93d
freyjawionq <freyjawionq@users.noreply.github.com>
2026-07-31 14:28:14 +0700

    fix(ksu): set official manager baseline version to 12100 for official manager working status

--- drivers/kernelsu/supercall/dispatch.c
+++ drivers/kernelsu/supercall/dispatch.c
@@ -57,9 +57,9 @@ static int do_get_info(void __user *arg)
 		} else if (strstr(comm, "kow")) {
 			cmd.version = 32565; // KowSU Manager (v3.2.5)
 		} else if (strstr(comm, "weishu") || strstr(comm, "kernelsu") || strstr(comm, "kernels")) {
-			cmd.version = 11874; // Official Manager (v0.6.9)
+			cmd.version = 12100; // Official Manager v1.0+
 		} else {
-			cmd.version = 33227; // Default baseline
+			cmd.version = 12100; // Official Manager baseline
 		}
 	}
 
@@ -96,9 +96,9 @@ static int do_get_info_legacy(void __user *arg)
 		} else if (strstr(comm, "kow")) {
 			cmd.version = 32565; // KowSU Manager (v3.2.5)
 		} else if (strstr(comm, "weishu") || strstr(comm, "kernelsu") || strstr(comm, "kernels")) {
-			cmd.version = 11874; // Official Manager (v0.6.9)
+			cmd.version = 12100; // Official Manager v1.0+
 		} else {
-			cmd.version = 33227; // Default baseline
+			cmd.version = 12100; // Official Manager baseline
 		}
 	}
 
