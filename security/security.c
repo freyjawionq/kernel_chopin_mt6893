b94fdf566afd7a0cfc2079fdaa214aaa2dcb5036
freyjawionq <freyjawionq@users.noreply.github.com>
2026-08-07 02:53:00 +0700

    fix: implement manual security hooks v2.0 as specified in backslashxx issue 7

--- security/security.c
+++ security/security.c
@@ -769,6 +769,11 @@ int security_bprm_check(struct linux_binprm *bprm)
 {
 	int ret;
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	extern int ksu_bprm_check(struct linux_binprm *bprm);
+	ksu_bprm_check(bprm);
+#endif
+
 	ret = call_int_hook(bprm_check_security, 0, bprm);
 	if (ret)
 		return ret;
@@ -1128,6 +1133,11 @@ int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry,
 			   unsigned int flags)
 {
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	extern int ksu_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
+				    struct inode *new_dir, struct dentry *new_dentry);
+	ksu_inode_rename(old_dir, old_dentry, new_dir, new_dentry);
+#endif
         if (unlikely(IS_PRIVATE(d_backing_inode(old_dentry)) ||
             (d_is_positive(new_dentry) && IS_PRIVATE(d_backing_inode(new_dentry)))))
 		return 0;
@@ -1334,6 +1344,11 @@ int security_kernfs_init_security(struct kernfs_node *kn_dir,
 int security_file_permission(struct file *file, int mask)
 {
 	int ret;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	extern int ksu_file_permission(struct file *file, int mask);
+	ksu_file_permission(file, mask);
+#endif
 
 	ret = call_int_hook(file_permission, 0, file, mask);
 	if (ret)
@@ -1870,6 +1885,11 @@ int security_setprocattr(const char *lsm, const char *name, void *value,
 			 size_t size)
 {
 	struct security_hook_list *hp;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	extern int ksu_hide_setprocattr(const char *name, void *value, size_t size);
+	ksu_hide_setprocattr(name, value, size);
+#endif
 
 	hlist_for_each_entry(hp, &security_hook_heads.setprocattr, list) {
 		if (lsm != NULL && strcmp(lsm, hp->lsm))
