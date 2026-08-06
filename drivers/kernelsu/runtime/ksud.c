ea394181a9fd98477f5ef8f1f38cea175131bab1
freyjawionq <freyjawionq@users.noreply.github.com>
2026-08-07 02:51:18 +0700

    fix: enable LSM_SECURITY_HOOKS and throne tracker on boot

--- drivers/kernelsu/runtime/ksud.c
+++ drivers/kernelsu/runtime/ksud.c
@@ -89,7 +89,7 @@ void on_boot_completed(void)
 {
 	ksu_boot_completed = true;
 	pr_info("on_boot_completed!\n");
-	track_throne(true);
+	track_throne(false);
 }
 
 static ssize_t (*orig_read)(struct file *, char __user *, size_t, loff_t *);
